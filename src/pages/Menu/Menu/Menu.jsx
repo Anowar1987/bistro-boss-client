7bb319fbf875a8afa03cc62661c23bea705fa2d3
Anowar1987 <anowarhossen.iim@gmail.com>
2024-05-17 20:05:04 +0600

    updated

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,12 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladsImg from '../../../assets/menu/salad-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
     return (
@@ -6,7 +14,16 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro Boss | Menu </title>
             </Helmet>
-            <h2> Menu Menu Menu Menu</h2>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={dessertImg} title="Desserts"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={pizzaImg} title="pizza"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={saladsImg} title="Salads"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={soupImg} title="Soup"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
